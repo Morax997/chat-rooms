66bec3e37788cb84cdd2e50b758f9a061db7dca1
Morax997 <71130183+Morax997@users.noreply.github.com>
2024-05-10 22:17:40 +0200

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBwnF0EG9514xYgqO-hpD3crE3lX31g2-o",
+    authDomain: "chat-rooms-app-6aba3.firebaseapp.com",
+    projectId: "chat-rooms-app-6aba3",
+    storageBucket: "chat-rooms-app-6aba3.appspot.com",
+    messagingSenderId: "782342334945",
+    appId: "1:782342334945:web:ae02e696b02b92d3b70aa0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
